6fe7519ba181515b093ac8bdf830262c99a747ee
Lauralynn <lauralynn1994@yahoo.com>
2022-04-10 22:38:24 -0500

    Challenge First draft

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -26,42 +26,43 @@ browser = Browser('chrome', **executable_path, headless=False)
 
 # In[46]:
 
+def mars_news():
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
-
-
-# In[ ]:
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
 
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+    # In[ ]:
 
 
-# In[ ]:
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    slide_elem = news_soup.select_one('div.list_text')
 
 
-slide_elem.find('div', class_='content_title')
+    # In[ ]:
 
 
-# In[ ]:
+    slide_elem.find('div', class_='content_title')
 
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+    # In[ ]:
 
 
-# In[ ]:
+    # Use the parent element to find the first `a` tag and save it as `news_title`
+    news_title = slide_elem.find('div', class_='content_title').get_text()
+    news_title
 
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # In[ ]:
+
+
+    #   Use the parent element to find the paragraph text
+    news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    news_p
 
 
 # ### Featured Image 
@@ -102,6 +103,7 @@ df
 
 
 # In[ ]:
+
 
 
 df.to_html()
